931946fd59791a8f84dc3646fea4553593a70fb7
Griffin Sorrentino <GriffinSorrentino@gmail.com>
2019-04-04 18:59:48 -0400

    updated sql update script

--- databases/v2019.2.1.0.update.sql
+++ databases/v2019.2.1.0.update.sql
@@ -1,8 +1,9 @@
-UPDATE scout_cards,
-    matches
-SET
-    scout_cards.MatchId = matches.key
-WHERE
-    scout_cards.matchid = matches.MatchNumber
-        AND scout_cards.EventId = matches.eventid
-        AND matches.matchtype = 'qm';
+UPDATE scout_cards, matches
+SET scout_cards.MatchId = matches.key
+WHERE scout_cards.matchid = matches.MatchNumber
+AND scout_cards.EventId = matches.eventid
+AND matches.matchtype = 'qm';
+
+ALTER TABLE scout_cards
+DROP COLUMN `RedAllianceFinalScore`,
+DROP COLUMN `BlueAllianceFinalScore`;
