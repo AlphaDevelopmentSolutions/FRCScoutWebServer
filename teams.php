6ce20aee01482fa408cd65b4d99397140965eaf5
Griffin Sorrentino <griffinsorrentino@gmail.com>
2019-04-24 00:10:14 -0400

    fixed issue on teams.php page with spacing

--- teams.php
+++ teams.php
@@ -55,9 +55,10 @@ $event = Events::withId($eventId);
 <body class="mdl-demo mdl-color--grey-100 mdl-color-text--grey-700 mdl-base">
 <div class="mdl-layout mdl-js-layout mdl-layout--fixed-header">
     <header class="mdl-layout__header mdl-layout__header--scroll mdl-color--primary">
-
         <div class="mdl-layout--large-screen-only mdl-layout__header-row">
             <?php include_once('includes/login-form.php') ?>
+        </div>
+        <div class="mdl-layout--large-screen-only mdl-layout__header-row">
             <h3><?php echo $event->Name; ?></h3>
         </div>
 
