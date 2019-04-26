836cd0f8488d549aa23ad45b14481f6937aa7a8c
Griffin Sorrentino <griffinsorrentino@gmail.com>
2019-04-25 20:38:31 -0400

    added link to go to home page with the header title

--- classes/Header.php
+++ classes/Header.php
@@ -33,7 +33,7 @@ class Header
             '
             <header class="mdl-layout__header mdl-layout__header--scroll mdl-color--primary">
                 <div class="mdl-layout__header-row">
-                    <span class="mdl-layout-title header-title">' . $this->Title . '</span>
+                    <span class="mdl-layout-title header-title"><a style="text-decoration: none; color: white;" href="/">' . $this->Title . '</a></span>
                     <div class="mdl-layout-spacer"></div>
                     <div class="version">Version ' . VERSION . '</div>
                 </div>
@@ -70,8 +70,30 @@ class Header
                         <a href="/match-overview.php?eventId=' . $this->EventId . '" class="mdl-navigation__link">Matches</a>
                         <a href="/teams.php?eventId=' . $this->EventId . '" class="mdl-navigation__link">Teams</a>
                         <a href="/stats.php?eventId=' . $this->EventId . '" class="mdl-navigation__link ">Stats</a>
-                    </nav>
-                    ' . 'LOGIN FORM GOES HERE' . '
+                    </nav>' .
+                ((!loggedIn()) ?
+                    '<form  action="login.php" method="post" style="padding: 1.5em; position: absolute !important; bottom: 0 !important; width: 197px;">
+                            <div class="mdl-textfield mdl-js-textfield">
+                                <input class="mdl-textfield__input" type="text" name="username" style="background-color: white !important; color: black; ">
+                                <label class="mdl-textfield__label" for="username" style="padding-left: .5em; padding-right: .5em; ">Username</label>
+                            </div>
+                            <br>
+                            <div class="mdl-textfield mdl-js-textfield">
+                                <input class="mdl-textfield__input" type="password" name="password" style="background-color: white !important; color: black;">
+                                <label class="mdl-textfield__label" for="password" style="padding-left: .5em; padding-right: .5em; ">Password</label>
+                            </div>    <br>
+                            <button class="mdl-button mdl-js-button mdl-button--raised mdl-js-ripple-effect mdl-button--accent" style="width: 100%;">
+                                Login
+                            </button>
+                            <input type="hidden" name="url" value="' . $_SERVER['REQUEST_URI'] . '">
+                        </form>'
+                    :
+                    '<form action="/logout.php?" style="margin: 1.5em; position: absolute !important; bottom: 0 !important;" method="post">
+                        <button class="mdl-button mdl-js-button mdl-button--raised mdl-js-ripple-effect mdl-button--accent" style="width: 199px;">
+                        Logout
+                        </button>
+                        <input type="hidden" name="url" value="' . $_SERVER['REQUEST_URI'] . '">
+                    </form>') . '
                 </div>
                 ';
 
@@ -80,3 +102,5 @@ class Header
     }
 
 }
+
+?>
