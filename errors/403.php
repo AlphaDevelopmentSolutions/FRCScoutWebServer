7a8191c667059bfc887295558ea242493098b4b3
Griffin Sorrentino <griffinsorrentino@gmail.com>
2019-09-28 16:08:07 -0400

    added http error pages

---
+++ errors/403.php
@@ -0,0 +1,30 @@
+<?php
+define('ROBOT_MEDIA_DIR', __DIR__ . '/assets/robot-media/originals/');
+define('ROBOT_MEDIA_THUMBS_DIR', __DIR__ . '/assets/robot-media/thumbs/');
+define('ROBOT_MEDIA_URL', '/assets/robot-media/originals/');
+define('ROBOT_MEDIA_THUMBS_URL', '/assets/robot-media/thumbs/');
+define('YEAR_MEDIA_URL', '/assets/year-media/');
+define('IMAGES_URL', '/assets/images/');
+define('CSS_URL', '/css/');
+define('JS_URL', '/js/');
+?>
+<!doctype html>
+<html lang="en">
+<head>
+    <title>403 - You shall not pass</title>
+    <?php require_once('../includes/meta.php') ?>
+</head>
+<body class="mdl-demo mdl-color--grey-100 mdl-color-text--grey-700 mdl-base">
+<div class="mdl-layout mdl-js-layout mdl-layout--fixed-header">
+    <main class="mdl-layout__content homescreen">
+        <div class="home-login">
+            <img src="https://emojipedia-us.s3.dualstack.us-west-1.amazonaws.com/thumbs/120/google/223/raised-hand_270b.png" width="200">
+            <h1 style="color: white">403 - Forbidden</h1>
+            <h3 style="text-align: center; color: white;">You shall not pass.</h3>
+        </div>
+    </main>
+</div>
+<?php require_once('../includes/bottom-scripts.php') ?>
+</body>
+</html>
+
