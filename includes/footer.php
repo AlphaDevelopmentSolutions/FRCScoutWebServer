d722d21aa02b2ba4ef22770eb98da885e901e154
Griffin Sorrentino <griffinsorrentino@gmail.com>
2019-10-20 15:43:46 -0400

    updated contact email and broken links

--- includes/footer.php
+++ includes/footer.php
@@ -1,8 +1,8 @@
 <div class="footer-text material-top-bottom-padding">
-    <a target="_blank" href="https://github.com/AlphaDevelopmentSolutions/FRCScout-Web">Contribute</a> -
+    <a target="_blank" href="https://github.com/FRCScout">Contribute</a> -
     <a target="_blank" href="/privacy.html">Privacy Policy</a> -
     <a target="_blank" href="/terms.html">Terms of Use</a> -
-    <a href="mailto:alphadevelopmentsolutions@gmail.com">Contact</a>
+    <a href="mailto:hello@frcscout.com">Contact</a>
 
     <br>
     <a href="#">v<?php echo VERSION ?></a>
